481eae068d5a39b18934a220ccd962cbd223f6f0
Tiago Dinis <tiagodinis33@proton.me>
2025-11-21 17:04:04 +0000

    feat(gl generator): add extensions required for importing dma-buf textures

--- build.rs
+++ build.rs
@@ -7,7 +7,12 @@ fn main() {
     let dest = env::var("OUT_DIR").unwrap();
     let mut file = File::create(Path::new(&dest).join("gl_bindings.rs")).unwrap();
 
-    Registry::new(Api::Gles2, (3, 2), Profile::Core, Fallbacks::All, [])
+    Registry::new(Api::Gles2, (3, 3), Profile::Core, Fallbacks::All, [
+	    "GL_OES_EGL_image",
+	    "GL_OES_EGL_image_external",
+	    "GL_EXT_memory_object_fd",
+	    "GL_EXT_semaphore_fd"
+    ])
         .write_bindings(StructGenerator, &mut file)
         .unwrap();
 }
